13751116cce11a6ea6027dd46d3cd999ebeafd64
김민서 <gimm08377@gmail.com>
2025-01-13 10:55:15 +0900

    [level 2] Title: 프로세스, Time: 1.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,31 @@
+from collections import deque
+# 특정 프로세스가 몇 번째로 실행되는지 반환
+# 우선순위 큐
+def solution(priorities, location):
+    answer = 0
+    queue = deque()
+    
+    max_priority = max(priorities)
+    for index, value in enumerate(priorities):
+        queue.append((value, index))
+    # print(queue) # deque([(1, 0), (1, 1), (9, 2), (1, 3), (1, 4), (1, 5)])
+    count = 1
+    while queue:
+        value, index = queue.popleft()
+        
+        if location == index:
+            answer = count
+        
+        if value == max_priority:
+            # print(value, index)
+            count += 1
+            if queue:
+                max_priority = max(queue, key=lambda x:x[0])[0]
+        else:
+            # 다시 추가
+            queue.append((value, index))
+			
+            
+        # print(queue)
+
+    return answer
