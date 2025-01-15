72d5c6321939f59600d86c7a53c06efe0af845c8
김민서 <gimm08377@gmail.com>
2025-01-15 10:16:49 +0900

    [level 1] Title: 폰켓몬, Time: 1.03 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,4 @@
 def solution(nums):
-    dict = {key: 0 for key in nums}
     n = len(nums)/2  
-    for num in nums:
-        dict[num] += 1
-    count = len([value for value in dict.values() if value != 0])
-    answer = min(count, n)
+    answer = min(len(set(nums)), n)
     return answer
